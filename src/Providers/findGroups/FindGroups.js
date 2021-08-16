b121082b6adb051ef5b3c096964cf4945053d358
Mônica S <monica.s.bergues@gmail.com>
2021-08-15 21:50:55 -0300

    feat: findGroup card, and fix findGroup provider

--- src/Providers/findGroups/FindGroups.js
+++ src/Providers/findGroups/FindGroups.js
@@ -1,33 +1,44 @@
 import { createContext, useState, useEffect, useContext } from "react";
-import { toast } from "react-toastify";
+
 import api from "../../Services/api";
 
 const FindGroup = createContext();
 
 export const FindGroupProvider = ({ children }) => {
-  const [findGroups, setFindGroups] = useState();
+  const [groupsList, setGroupsList] = useState();
+
+  const [token] = useState(localStorage.getItem("token") || "");
 
   useEffect(() => {
     api
-      .get(`/groups/`)
-      .then((response) => setFindGroups(response.data.results))
-      .catch((err) => toast.error("Grupo não encontrado!"));
+      .get(`/groups/?page=2`, "", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => setGroupsList(response.data.results))
+      .catch((err) => console.log(err));
   }, []);
 
+  const Subscribe = (data) => {
+    api
+      .post(`/groups/${data}/subscribe/`, "", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
+
+  console.log(groupsList);
   return (
-    <FindGroup.Provider value={{ findGroups, setFindGroups }}>
+    <FindGroup.Provider value={{ groupsList, Subscribe }}>
       {children}
     </FindGroup.Provider>
   );
 };
 
-export const useGroupList = () => {
-  const context = useContext(FindGroup);
-
-  const { findGroups, setFindGroups } = context;
-
-  return {
-    findGroups,
-    setFindGroups,
-  };
-};
+export const useGroupList = () => useContext(FindGroup);
