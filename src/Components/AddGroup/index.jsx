ba55444b45981c14ec42fd457e722a0d87c1d765
Mônica S <monica.s.bergues@gmail.com>
2021-08-15 21:58:48 -0300

    feat: addGroup card and fix addGoup provider

---
+++ src/Components/AddGroup/index.jsx
@@ -0,0 +1,33 @@
+import { Card, Input, Button, Form } from "antd";
+import { useAddNewGroup } from "../../Providers/addGroups/AddGroups";
+import { Container } from "./sytle";
+const { TextArea } = Input;
+export const AddGroupCard = () => {
+  const { AddNewGroup } = useAddNewGroup();
+
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    AddNewGroup(values);
+  };
+
+  return (
+    <Container>
+      <Card title="Crie um Grupo">
+        <Form form={form} name="control-hooks" onFinish={onFinish}>
+          <Form.Item name="name">
+            <Input placeholder="Nome do grupo" />
+          </Form.Item>
+          <Form.Item name="category">
+            <Input placeholder="Categoria" />
+          </Form.Item>
+          <Form.Item name="description">
+            <TextArea placeholder="Descrição" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Enviar
+          </Button>
+        </Form>
+      </Card>
+    </Container>
+  );
+};
