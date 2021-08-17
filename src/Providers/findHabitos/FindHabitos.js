6f56c118c7272435158adad201d4fecffb06721e
Weslley Bastos <wessbastos@gmail.com>
2021-08-16 23:16:08 -0300

    buid: component myhabbits; fix: provider findHabitos

--- src/Providers/findHabitos/FindHabitos.js
+++ src/Providers/findHabitos/FindHabitos.js
@@ -1,24 +1,30 @@
 import { useEffect, useState, createContext, useContext } from "react";
 import axios from "axios";
-
-//receber a criação dos hábitos
-//receber a atualização dos habitos
-//receber tolken do usuário
+import { useAuth } from "../auth/Auth";
+import { useAddHabit } from "../addHabitos/AddHabitos";
 
 const findHabitsContext = createContext();
 
 export const FindHabitsProvider = ({children}) => {
+
     const [findHabits, setFindHabits] = useState([]);
-    
+    const { userID } = useAuth();
+    const { newHabbit } = useAddHabit();
+
     useEffect(()=> {
-        axios.get("https://kabit-api.herokuapp.com/habits/personal/")
-        .then(response => setFindHabits(response.data))
+        axios
+        .get("https://kabit-api.herokuapp.com/habits/personal/", {
+            headers: {
+                Authorization: `Bearer ${userID}`,
+            },
+        })
+        .then(response => setFindHabits([...newHabbit, response.data.results]))
         .catch(err => alert(err + "not found"))
 
     }, [])
     
     return (
-        <findHabitsContext.Provider value={{findHabits}}>
+        <findHabitsContext.Provider value={{findHabits, newHabbit}}>
             {children}
         </findHabitsContext.Provider>
     )
