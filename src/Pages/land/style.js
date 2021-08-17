cbc61b7cb73b60481462ebc0efbb6c7d378eaefa
joaopedro <jpss7693@gmai.com>
2021-08-17 13:55:02 -0300

    feat: responsividade na land page

--- src/Pages/land/style.js
+++ src/Pages/land/style.js
@@ -11,7 +11,7 @@ export const Body = styled.body`
   background-size: cover;
   justify-content: space-around;
 
-  @media (max-width: 1148px) {
+  @media (max-width: 1024px) {
     background-image: url(${fundomobile});
     background-repeat: no-repeat;
     background-size: cover;
@@ -23,11 +23,11 @@ export const Body = styled.body`
 export const Titulo = styled.h2`
   color: #f7e7dc;
   font-family: "Alfa Slab One", cursive;
-  font-size: 95px;
+  font-size: 92px;
   margin-left: 90px;
   text-shadow: 2.5px 2.5px black;
 
-  @media (max-width: 1148px) {
+  @media (max-width: 1024px) {
     font-size: 50px;
     margin: 140px 0 0 0;
     text-align: center;
@@ -38,21 +38,21 @@ export const Box = styled.div`
   text-align: center;
   margin-right: 50px;
 
-  @media (max-width: 1148px) {
+  @media (max-width: 1024px) {
     margin: 130px auto;
   }
 `;
 
 export const Paragrafo = styled.p`
-  font-size: 31px;
-  width: 504px;
+  font-size: 29px;
+  width: 494px;
   color: white;
   height: 287px;
   font-family: "Fauna One", serif;
   text-align: center;
   margin-bottom: 50px;
 
-  @media (max-width: 1148px) {
+  @media (max-width: 1024px) {
     display: none;
   }
 `;
@@ -69,7 +69,7 @@ export const Button = styled.button`
   font-family: "Fauna One", serif;
   cursor: pointer;
 
-  @media (max-width: 1148px) {
+  @media (max-width: 1024px) {
     width: 140px;
     height: 43px;
     border-radius: 10px;
